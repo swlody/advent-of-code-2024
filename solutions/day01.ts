347afee7aef4327255236fe8eb292ea32c9a413b
Sam Wlody <swlody@gmail.com>
2025-01-14 18:16:15 -0500

    day 3 - dumb
    naming consistency

--- solutions/day01.ts
+++ solutions/day01.ts
@@ -1,10 +1,10 @@
 import { assertEquals } from "@std/assert";
 
-export function parse_input(input_string: string): [number[], number[]] {
+function parse_input(input: string): [number[], number[]] {
   const left_list = [];
   const right_list = [];
 
-  for (const line of input_string.split("\n")) {
+  for (const line of input.split("\n")) {
     const [left, right] = line.split("   ");
     left_list.push(parseInt(left));
     right_list.push(parseInt(right));
@@ -13,45 +13,49 @@ export function parse_input(input_string: string): [number[], number[]] {
   return [left_list, right_list];
 }
 
-export function solve_part1(left_list: number[], right_list: number[]): number {
-  left_list.sort();
-  right_list.sort();
+function solve_part1(left: number[], right: number[]): number {
+  left.sort();
+  right.sort();
 
   let total_distance = 0;
-  for (let i = 0; i < left_list.length; i++) {
-    const distance = Math.abs(left_list[i] - right_list[i]);
+  for (let i = 0; i < left.length; i++) {
+    const distance = Math.abs(left[i] - right[i]);
     total_distance += distance;
   }
 
   return total_distance;
 }
 
-export function solve_part2(left_list: number[], right_list: number[]): number {
+function solve_part2(left: number[], right: number[]): number {
   const right_counts = new Map();
-  for (const right of right_list) {
-    right_counts.set(right, (right_counts.get(right) ?? 0) + 1);
+  for (const right_value of right) {
+    right_counts.set(right_value, (right_counts.get(right_value) ?? 0) + 1);
   }
 
   let similarity_score = 0;
-  for (const left of left_list) {
-    const right_count = right_counts.get(left) ?? 0;
-    similarity_score += left * right_count;
+  for (const left_value of left) {
+    const right_count = right_counts.get(left_value) ?? 0;
+    similarity_score += left_value * right_count;
   }
 
   return similarity_score;
 }
 
 if (import.meta.main) {
-  const input_string = Deno.readTextFileSync("./input/day01.txt");
-  const [left_list, right_list] = parse_input(input_string);
+  const input = Deno.readTextFileSync("./input/day01.txt");
+  const [left, right] = parse_input(input);
   console.log("Day 1 Part 1:");
-  console.log(solve_part1(left_list, right_list));
+  console.log(solve_part1(left, right));
 
   console.log("Day 1 Part 2:");
-  console.log(solve_part2(left_list, right_list));
+  console.log(solve_part2(left, right));
 }
 
-const test_input_string = `3   4
+//
+// Tests
+//
+
+const test_input = `3   4
 4   3
 2   5
 1   3
@@ -59,13 +63,13 @@ const test_input_string = `3   4
 3   3`;
 
 Deno.test(function test_part2() {
-  const [left_list, right_list] = parse_input(test_input_string);
+  const [left_list, right_list] = parse_input(test_input);
   const solution = solve_part1(left_list, right_list);
   assertEquals(11, solution);
 });
 
 Deno.test(function test_part2() {
-  const [left_list, right_list] = parse_input(test_input_string);
+  const [left_list, right_list] = parse_input(test_input);
   const solution = solve_part2(left_list, right_list);
   assertEquals(31, solution);
 });
